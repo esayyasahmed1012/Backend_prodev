cca6349358ef4f35d0f3cc3bea6a6f8eb543a2c6
esayyas ahmed <esayyasahmed1012@gmail.com>
2025-12-07 22:26:35 +0300

    updated

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -13,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
             'user_id', 
             'email',
             'first_name', 
-            'last_name'
+            'last_name',
             'fullname',
             'role',
             'role_display',
@@ -36,19 +36,24 @@ class MessageSerializer(serializers.ModelSerializer):
         read_only_fields = ['message_id', 'sent_at']
     def validate(self, data):
         request=self.context.get('request')
+        if not request or not request.user.is_authenticated:
+            raise serializers.ValidationError("User musst be logged in.")
+        sender_id=data.get('sender_id')
+        if str(request.user.user_id) != str(sender_id):
+            raise serializers.ValidationError("You cannot send messages as someone else!")
+        conversation=data.get('conversation')
         if request and request.method in ['POST', 'PUT', 'PATCH']:
-            conversation=data.get('conversation')
-            sender_id=data.get('sender_id')
             if not conversation.participants.filter(user_id=sender_id).exists():
                 raise serializers.ValidationError("Sender must be a participant in the conversation.")
         return data
-class ConversationSerializer(serializers.ModelSerializer):
-    parcicipants =UserSerializer(many=True, read_only=True)
-    parcicipant_id=serializers.PrimaryKeyRelatedField(
+class ConversationListSerializer(serializers.ModelSerializer):
+    participants =UserSerializer(many=True, read_only=True)
+    participant_id=serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all(),
         many=True,
-        Write_only=True,
-        required=False
+        write_only=True,
+        required=False,
+        source='participants'
     )
     last_message=serializers.SerializerMethodField()
     unread_count=serializers.SerializerMethodField()
@@ -56,16 +61,32 @@ class ConversationSerializer(serializers.ModelSerializer):
         model=Conversation
         fields=[
             'conversation_id',
-            'parcicipants',
-            'parcicipant_id',
+            'participants',
+            'participant_id',
             'created_at',
             'last_message',
             'unread_count',
         ]
         read_only_fields = ['conversation_id', 'created_at']
     def get_last_message(self, obj):
+        msg=obj.messages.select_related('sender').order_by('-sent_at').first()
+        return MessageSerializer(msg, context=self.context).data if msg else None
+    def get_unread_count(self, obj):
         request=self.context.get('request')
         if request and request.user.is_authenticated:
             return obj.messages.filter(
-                sent_at__get=timezone.now() - timezone.timedelta(days=7)
-            )
+                sent_at__gte=timezone.now() - timezone.timedelta(days=30)
+            ).exclude(sender=request.user).count()
+        return 0
+class converssationDetailSerializer(serializers.ModelSerializer):
+    participants=UserSerializer(many=True, read_only=True)
+    messages=MessageSerializer(many=True, read_only=True)
+    class Meta:
+        model=Conversation
+        fields=[
+            'conversation_id',
+            'participants',
+            'created_at',
+            'messages',
+        ]
+        read_only_fields = ['conversation_id', 'created_at', 'participants', 'messages']
